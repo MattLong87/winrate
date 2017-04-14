d2f3b704ceaad61331d50b1605075fa006763c7d
Matthew Long <MattLong87@gmail.com>
2017-04-14 14:05:29 -0400

    Started adding test for dashboard api endpoint

--- test/test-winrate-api.js
+++ test/test-winrate-api.js
@@ -50,3 +50,24 @@ describe("Sessions page", function(){
 			})
 	})
 })
+
+describe("Dashboard API endpoint", function(){
+	it("Should return information for the dashboard and 4 most recent sessions", function(){
+		let keys = ["username", "myFirstName", "recentPlayers", "sessions"];
+		let sessionKeys = ["game", "players", "winner", "timeStamp"];
+		return chai.request(app)
+			.get('/api/dashboardInfo')
+			.then(function(res){
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a("object");
+				res.body.sessions.should.have.length.of(4);
+				res.body.should.have.all.keys(keys);
+				res.body.sessions.forEach(function(session){
+					session.should.be.a("object");
+					session.should.have.all.keys(sessionKeys);
+					//add checks for each session field
+				})
+			})
+	})
+})
