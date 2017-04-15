1b8053337d7959cac647d53861793e593f48e18e
Matthew Long <MattLong87@gmail.com>
2017-04-14 23:56:34 -0400

    Began implementing patch route for adding sessions

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -6,6 +6,14 @@ const jsonParser = bodyParser.json();
 
 const {User, userSchema} = require('../models/models.js');
 
+router.get("/", (req, res) => {
+	User.findOne()
+	.exec()
+	.then((user) => {
+		res.send(user);
+	})
+})
+
 //This automatically makes a route for each
 //method on the userSchema
 for (let method in userSchema.methods){
@@ -25,5 +33,20 @@ for (let method in userSchema.methods){
 // 			res.send(user.dashboardInfo());
 // 		})
 // })
+
+router.patch("/:id", (req, res) => {
+	//should do some validation on session fields and return helpful errors
+	let newSession = {};
+	newSession.game = req.body.game;
+	newSession.players = req.body.players;
+	newSession.winner = req.body.winner;
+	newSession.timeStamp = Date.now().toString();
+
+	User.update({_id: req.params.id}, {$push: {sessions: newSession}})
+	.exec()
+	.then(() => res.status(204).end());
+})
+
+
 
 module.exports = router;
