f9d2a0ba478c325792eed0ebedc2294ba95355df
Matthew Long <MattLong87@gmail.com>
2017-04-21 01:09:32 -0400

    model cleanup and dashboard styling

--- models/models.js
+++ models/models.js
@@ -17,12 +17,11 @@ const userSchema = mongoose.Schema({
 
 userSchema.methods.dashboardInfo = function(){
 	return {
-		// username: this.username,
-		// myFirstName: this.name.firstName,
-		// overallWinrate: this.overallWinrate,
-		// recentPlayers: this.players.slice(-5).reverse(),
-		// recentSessions: this.sessions.slice(-4).reverse(),
-		recentSessions: this.sessions[0]._id
+		username: this.username,
+		myFirstName: this.name.firstName,
+		overallWinrate: this.overallWinrate,
+		recentPlayers: this.players.slice(-5).reverse(),
+		recentSessions: this.sessions.slice(-4).reverse(),
 	}
 }
 
