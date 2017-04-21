072ed0aad618e7e9a690909b0596762bebe4d9b7
Matthew Long <MattLong87@gmail.com>
2017-04-21 11:44:24 -0400

    uncommenting tests

--- test/test-winrate-api.js
+++ test/test-winrate-api.js
@@ -16,49 +16,49 @@ function tearDownDb() {
     return mongoose.connection.dropDatabase();
 }
 
-// describe("Root URL", function(){
-// 	it("Should return a 200 status and HMTL", function(){
-// 		return chai.request(app)
-// 			.get('/')
-// 			.then(function(res){
-// 				res.should.have.status(200);
-// 				res.should.be.html;
-// 			})
-// 	})
-// })
+describe("Root URL", function(){
+	it("Should return a 200 status and HMTL", function(){
+		return chai.request(app)
+			.get('/')
+			.then(function(res){
+				res.should.have.status(200);
+				res.should.be.html;
+			})
+	})
+})
 
-// describe("Dashboard page", function(){
-// 	it("Should return a 200 status and HMTL", function(){
-// 		return chai.request(app)
-// 			.get('/dashboard.html')
-// 			.then(function(res){
-// 				res.should.have.status(200);
-// 				res.should.be.hmtl;
-// 			})
-// 	})
-// })
+describe("Dashboard page", function(){
+	it("Should return a 200 status and HMTL", function(){
+		return chai.request(app)
+			.get('/dashboard.html')
+			.then(function(res){
+				res.should.have.status(200);
+				res.should.be.hmtl;
+			})
+	})
+})
 
-// describe("Add-Session page", function(){
-// 	it("Should return a 200 status and HMTL", function(){
-// 		return chai.request(app)
-// 			.get('/add-session.html')
-// 			.then(function(res){
-// 				res.should.have.status(200);
-// 				res.should.be.hmtl;
-// 			})
-// 	})
-// })
+describe("Add-Session page", function(){
+	it("Should return a 200 status and HMTL", function(){
+		return chai.request(app)
+			.get('/add-session.html')
+			.then(function(res){
+				res.should.have.status(200);
+				res.should.be.hmtl;
+			})
+	})
+})
 
-// describe("Sessions page", function(){
-// 	it("Should return a 200 status and HMTL", function(){
-// 		return chai.request(app)
-// 			.get('/sessions.html')
-// 			.then(function(res){
-// 				res.should.have.status(200);
-// 				res.should.be.hmtl;
-// 			})
-// 	})
-// })
+describe("Sessions page", function(){
+	it("Should return a 200 status and HMTL", function(){
+		return chai.request(app)
+			.get('/sessions.html')
+			.then(function(res){
+				res.should.have.status(200);
+				res.should.be.hmtl;
+			})
+	})
+})
 
 describe("Winrate API resource", function(){
 	before(function() {
